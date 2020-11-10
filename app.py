ddb4e1ecf3af2a963cf98813c527526623bc03e5
David-Lisboa <dlrlisboa@gmail.com>
2020-11-10 13:54:12 -0400

    Criação das pasta do projeto

--- app.py
+++ app.py
@@ -3,4 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!!'
+        # Tira o espaço e coloca em minusculo
+    estilo = input("Informe o seu estilo: ").strip().lower()
+
+    if estilo == "medieval":
+         return "espada"
+
+    elif estilo == "futurista":
+        return "sabre de luz"
+
+    else:
+        return "erro"
+
+# http://127.0.0.1:5000/
